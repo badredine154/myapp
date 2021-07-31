fd3fa40fc37891a97d42fadf53b15b6b06e6fc4f
badredine154 <87574290+badredine154@users.noreply.github.com>
2021-07-31 03:29:17 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <div className="mb-3">
+            <label for="exampleFormControlInput1" className="form-label">Name</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+
+            <label for="exampleFormControlInput1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+
+
+          </div>
+        </form>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
